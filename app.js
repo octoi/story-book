9bd895a5b949f3fe5487fc3dc8d30ab35eb66091
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-03 13:20:19 +0530

    passport setup

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDb = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+
+require('./config/passport')(passport); // passport config
 
 connectDb();
 
@@ -18,6 +22,17 @@ if (process.env.NODE_ENV == 'development') {
 // Handlebars
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
+
+// sessions
+app.use(session({
+    secret: 'superSecretStuff',
+    resave: false,
+    saveUninitialized: false,
+}));
+
+// Passport mw
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(express.static(path.join(__dirname, 'public')))
 
