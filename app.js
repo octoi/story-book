21cb902c732ac76f713e4ab1b03d65e1c67e71f2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-03 11:35:44 +0530

    hbs setup

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 const connectDb = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
 
+connectDb();
+
 const app = express();
 
-connectDb();
+if (process.env.NODE_ENV == 'development') {
+    app.use(morgan('dev'));
+}
+
+// Handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view-engine', '.hbs');
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`[+] Server started at http://localhost:${port}/`));
