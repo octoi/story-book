c5abaab514331a6566825803b90ac4df1282d1b6
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-04 11:05:06 +0530

    displaying storis

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Story = require('../models/Story');
 const { ensureAuth, ensureGuest } = require('../middlewares/auth');
 
 router.get('/', ensureGuest, (req, res) => {
@@ -8,10 +9,19 @@ router.get('/', ensureGuest, (req, res) => {
     })
 })
 
-router.get('/dashboard', ensureAuth, (req, res) => {
-    res.render('dashboard', {
-        name: req.user.firstName,
-    })
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        });
+    } catch (err) {
+        console.log(`[-] Failed to fetch data of user ${req.user.id}`)
+        console.log(err)
+
+        res.render('error/500')
+    }
 })
 
 module.exports = router;
